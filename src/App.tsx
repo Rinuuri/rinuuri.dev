c57e377b5c80e814a0cb77c0016059fa69bf3f21
Rinuuri <github@rinuuri.dev>
2025-05-29 22:39:18 +0300

    Adding all the logos and the profile picture

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Background from "./components/Background";
 import "./main.css";
 import { init } from "./scripts/lang";
 
+
 export function App() {
   const canvasRef = useRef(null);
 
@@ -14,6 +15,13 @@ export function App() {
     <>
       <div id="language-div">
 				<canvas ref={canvasRef} id="language-renderer"></canvas>
+			</div>
+      <div id="profile-picture-div">
+				<div className="circular-mask profile-gradient">
+					<div className="circular-mask" style={{margin: "2.5px"}}>
+						<img src={require("./images/profile_picture.jpg")} style={{translate: "7.5% 7.5% 0px"}} alt="Profile picture" id="profile-picture-img" />
+					</div>
+				</div>
 			</div>
       <Background />
     </>
--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -12,6 +12,12 @@ export default function Background() {
         <div id="background">
             <div className="gradient-overlay">
                 <div style={{marginTop: "10%", marginLeft: "85%", animationDelay: "-1s"}}><img src={require("../images/android.svg")} /></div>
+                <div style={{marginTop: "5%", marginLeft: "77%", animationDelay: "-3s"}}><img src={require("../images/jetpack-compose.png")} /></div>
+                <div style={{marginTop: "20%", marginLeft: "6%", animationDelay: "-2s"}}><img src={require("../images/papermc.png")} /></div>
+                <div style={{marginTop: "42%", marginLeft: "20%", animationDelay: "-4s"}}><img src={require("../images/react.svg")} /></div>
+                <div style={{marginTop: "10%", marginLeft: "15%", animationDelay: "-5s"}}><img src={require("../images/folia.webp")} /></div>
+                <div style={{marginTop: "45%", marginLeft: "70%", animationDelay: "-2s"}}><img src={require("../images/javascript.svg")} /></div>
+                <div style={{marginTop: "30%", marginLeft: "90%", animationDelay: "-4s"}}><img src={require("../images/golang.svg")} /></div>
             </div>
             <canvas ref={canvasRef} id="background-renderer" />
         </div>
